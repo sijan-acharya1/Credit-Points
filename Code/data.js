614962a8bf5a7b758228129527597b03538e04fe
sijan-acharya21 <sijan.code@gmail.com>
2024-10-11 06:02:42 +1100

    Version 1

---
+++ Code/data.js
@@ -0,0 +1,35 @@
+// Credit account details
+const Credit = {
+  name: 'Sijan Acharya',
+  email: 'sijan.acharya@credit.com',
+  phone: '0412345678',
+  balance: 0,
+  reward: ['Apple', 'Microsoft', 'Google', 'Meta'],
+  transfer: {
+    // Check for patterns in the real world to predict consumer trends
+    transfer1: {
+      date: '8th October 2024', // Date function
+      company: 'PizzaHut',
+      item: 'Large Barbeque Pizza', // PizzaHut has 2 for 1 deals on Tuesday
+      quantity: 2,
+      location: 'Hurstville Westfield',
+      price: 18.95,
+    },
+    transfer2: {
+      date: '12th October 2024',
+      company: 'British Petroleum',
+      item: 'Oak Chocolate 700 mL drink',
+      quantity: 4,
+      location: 'Riverwood',
+      price: 24.6,
+    },
+    transfer3: {
+      date: '13th October 2024', // Date function
+      company: 'Woolworths',
+      item: 'Coca Cola 600 mL drink',
+      quantity: 1,
+      location: 'Hurstville Westfield',
+      price: 3.85,
+    },
+  },
+};
