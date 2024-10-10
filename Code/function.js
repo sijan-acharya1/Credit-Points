3082ede3a091ea70d80316a293c3a2dc34ff5d97
sijan-acharya21 <sijan.code@gmail.com>
2024-10-11 06:16:59 +1100

    Version 2

---
+++ Code/function.js
@@ -0,0 +1,24 @@
+// Buy function
+
+// Sell function
+
+// Exchange function
+
+// Transfer history
+
+// Analytics on consumer trends
+
+/*
+- Create an account for the user
+- Send Credit Points from one account to another account
+- Buy Credit Points
+- Sell Credit Points
+- Exchange Credit Points and other reward points
+
+Presentation
+
+- View account balance
+- Give analytics for account balance over the change in time
+- Detailed data on transfer details, spending patterns and behaviour
+
+*/
