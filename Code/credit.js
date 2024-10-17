12c86a2344925e711b2eb35055cd91975c6c4b87
sijan-acharya21 <sijan.code@gmail.com>
2024-10-18 00:31:21 +1100

    Created database in credit.js

--- Code/credit.js
+++ Code/credit.js
@@ -1,1 +1,17 @@
 // Create account
+const Database = {
+  Account: {
+    user1: {
+      name: 'Sijan Acharya',
+      balance: 100,
+    },
+    user2: {
+      name: 'Mark Zuckerberg',
+      balance: 100,
+    },
+    user3: {
+      name: 'Larry Ellison',
+      balance: 100,
+    },
+  },
+};
