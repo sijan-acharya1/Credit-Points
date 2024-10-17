378e6efe6ced854a37d81a6baf2fcbb47fd1d90a
sijan-acharya21 <sijan.code@gmail.com>
2024-10-18 00:42:55 +1100

    Added accounts to the database

--- Code/credit.js
+++ Code/credit.js
@@ -1,17 +1,17 @@
 // Create account
-const Database = {
-  Account: {
-    user1: {
-      name: 'Sijan Acharya',
-      balance: 100,
-    },
-    user2: {
-      name: 'Mark Zuckerberg',
-      balance: 100,
-    },
-    user3: {
-      name: 'Larry Ellison',
-      balance: 100,
-    },
+const Account = {
+  user1: {
+    name: 'Sijan Acharya',
+    balance: 100,
+  },
+  user2: {
+    name: 'Mark Zuckerberg',
+    balance: 100,
+  },
+  user3: {
+    name: 'Larry Ellison',
+    balance: 100,
   },
 };
+
+// Find the name of the user in the database
